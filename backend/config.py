e1afcd2839f29f59afcd65128128548e4976d51e
.email <jesicamitiap@.gamil.com>
2026-07-19 20:29:49 -0500

    Fix agregar atajo de usuario admin temporal en login

--- backend/config.py
+++ backend/config.py
@@ -7,6 +7,6 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'tu_clave_secreta_aqui')
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'emir'
-    MYSQL_PASSWORD = 'jessica2003'  # Tu contraseña de MySQL
+    MYSQL_PASSWORD = 'emir123'  # Tu contraseña de MySQL
     MYSQL_DB = 'control_insumos'
     MYSQL_PORT = 3306
